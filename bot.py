b3e52a24faf01059a3f5b9b8ad93a7bfa4ca0ceb
nawaf5noor5-cloud <nawaf5noor5@gmail.com>
2026-09-10 22:26:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s
 log = logging.getLogger(__name__)
 
 # الإعدادات والمتغيرات الأساسية
-TOKEN = "8968520359:AAGNBUm9GssXoB6SeZaUPH6IAfxC0aFJQo"
+TOKEN = "8968520359:AAE0L8vv6NEHL6cL4hn0KSRge_an17QAoAA"
 INITIAL_ADMIN_ID = "420693139"  # معرف المالك
 ALLOWED_USERS = {INITIAL_ADMIN_ID}
 admin_adding_state = set()
