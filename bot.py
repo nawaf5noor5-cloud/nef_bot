f80962ee03c22c62daea3f1e4ef856b07fef1619
nawaf5noor5-cloud <nawaf5noor5@gmail.com>
2026-09-18 15:35:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ log = logging.getLogger(__name__)
 
 # --- الإعدادات والمتغيرات العامة ---
 INITIAL_ADMIN_ID = "420693139"  # معرف المالك
-TOKEN = "8968520359:AAFvKf7M2lnhJpCZxzaHauwlQG16ClZqCqc"
+TOKEN = "8928448626:AAE6sxfOjTZV_AyuJEyyahg6rpwdXW8dsXg"
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 USERS_FILE = os.path.join(BASE_DIR, "allowed_users.json")
